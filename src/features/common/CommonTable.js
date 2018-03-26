72a1b81feb5e39ac181189c3129036b973264323
rchgonzaga@gmail.com <rchgonzaga@gmail.com>
2018-03-26 17:14:33 -0300

    updating crashy table component.

--- src/features/common/CommonTable.js
+++ src/features/common/CommonTable.js
@@ -1,15 +1,29 @@
 import React from 'react';
 import { Table } from 'semantic-ui-react';
 
-const row = (x, i, header) => (
-  <Table.Row key={`tr-${i}`}>{header.map((y, k) => <Table.Cell key={`trc-${k}`}>{x[y.prop]}</Table.Cell>)}</Table.Row>
-);
+const row = (x, i, header = {}) => {
+  if (!header) {
+    return <div>asdasd</div>;
+  }
 
-export default ({ data, header }) => (
-  <Table celled striped>
-    <Table.Header>
-      <Table.Row>{header.map((x, i) => <Table.HeaderCell key={`thc-${i}`}>{x.name}</Table.HeaderCell>)}</Table.Row>
-    </Table.Header>
-    <Table.Body>{data.map((x, i) => row(x, i, header))}</Table.Body>
-  </Table>
-);
+  return (
+    <Table.Row key={`tr-${i}`}>{header.map((y, k) => <Table.Cell key={`trc-${k}`}>{x[y.prop]}</Table.Cell>)}</Table.Row>
+  );
+};
+
+export default ({ data, header}) => {
+  if (!header) {
+    return <div>asdasd</div>;
+  }
+  if (!data) {
+    return <div>asdasd</div>;
+  }
+  return (
+    <Table celled striped>
+      <Table.Header>
+        <Table.Row>{header.map((x, i) => <Table.HeaderCell key={`thc-${i}`}>{x.name}</Table.HeaderCell>)}</Table.Row>
+      </Table.Header>
+      <Table.Body>{data.map((x, i) => row(x, i, header))}</Table.Body>
+    </Table>
+  );
+};
